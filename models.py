8c4f10e9c1d3fd7499422158507c897b1e44b72c
liam <liamceelent@gmail.com>
2021-06-25 14:40:05 +1200

    database commit

--- models.py
+++ models.py
@@ -7,5 +7,18 @@ class User(db.Model):
     salt = db.Column(db.String(225))
     key = db.Column(db.String(225))
 
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(225), unique=True)
+
+class Faction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(225), unique=True)
+
+class Location_Faction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lid = db.Column(db.Integer, db.ForeignKey('location.id'), nullable = False)
+    fid = db.Column(db.Integer, db.ForeignKey('faction.id'), nullable = False)
+    period = db.Column(db.Integer)
 
 db.create_all()
