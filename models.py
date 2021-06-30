f2d7c9ef19abb9845cf3d723e04d81fdd069a984
liam <liamceelent@gmail.com>
2021-06-30 14:34:47 +1200

    models added tables

--- models.py
+++ models.py
@@ -2,23 +2,50 @@ from main import db
 
 
 class User(db.Model):
+    __tablename__ = "User"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(225), unique=True)
     salt = db.Column(db.String(225))
     key = db.Column(db.String(225))
 
 class Location(db.Model):
+    __tablename__ = "Location"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(225), unique=True)
 
 class Faction(db.Model):
+    __tablename__ = "Faction"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(225), unique=True)
 
 class Location_Faction(db.Model):
+    __tablename__ = "Location_Faction"
     id = db.Column(db.Integer, primary_key=True)
-    lid = db.Column(db.Integer, db.ForeignKey('location.id'), nullable = False)
-    fid = db.Column(db.Integer, db.ForeignKey('faction.id'), nullable = False)
+    lid = db.Column(db.Integer, db.ForeignKey('Location.id'), nullable = False)
+    fid = db.Column(db.Integer, db.ForeignKey('Faction.id'), nullable = False)
     period = db.Column(db.Integer)
+
+class Famous_Viking(db.Model):
+    __tablename__ = "Famous_Viking"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(225), unique=True)
+    age = db.Column(db.Integer)
+    fid = db.Column(db.Integer, db.ForeignKey('Faction.id'), nullable = False)
+    wid = db.Column(db.Integer, db.ForeignKey('Weapon.id'), nullable = False)
+    kill = db.Column(db.Integer)
+
+class Weapon(db.Model):
+    __tablename__ = "Weapon"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(225), unique=True)
+
+Visted = db.Table('Visted', db.Model.metadata,
+    db.Column('Famous_id', db.Integer, db.ForeignKey('Famous_Viking.id')),
+    db.Column('Location_id', db.Integer, db.ForeignKey('Location.id'))
+)
+
+
+
+
 
 db.create_all()
