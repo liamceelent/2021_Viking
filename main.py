1f838b566e72eb2656702eee7f9a9d3079811653
liam <liamceelent@gmail.com>
2021-07-04 20:28:03 +1200

    new page

--- main.py
+++ main.py
@@ -18,13 +18,8 @@ def home():
     return render_template('home.html', page_title="Home")
 
 
-@app.route('/login')
-def login():
 
-    return render_template('login.html', page_title="Login")
-
-
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST', 'GET'])
 def login_post():
     if request.method == 'POST' and "name" in request.form:
 
@@ -32,8 +27,7 @@ def login_post():
         password = request.form['pass']
 
         name_check = models.User.query.filter_by(name=name).first()
-        print(name_check)
-        print("2")
+
         if name_check is not None:
             salt = name_check.salt
             key = name_check.key
@@ -47,39 +41,33 @@ def login_post():
         else:
             status = "Wrong user name or password."
             return render_template('login.html', status=status)
-
-    return render_template('login.html', page_title="Login")
-
-
-
-@app.route('/sign_up')
-def sign_up():
-
-    return render_template('sign.html', page_title="Sign_up")
-
-
-@app.route('/sign_up', methods=['POST'])
-def sign_up_post():
-
-    name = request.form['sign_name']
-    password = request.form['sign_pass']
-
-    name_check = models.User.query.filter_by(name=name).first()
-
-    if name_check is None:
-        salt = os.urandom(32)
-        key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
-
-        user = models.User(name=name, salt=salt, key=key)
-        db.session.add(user)
-        db.session.commit()
-        session['name'] = name
-        return redirect(url_for("home"))
     else:
-        status = "Name Already Taken"
-        return render_template('sign.html', page_title="Sign_up",status= status)
+        return render_template('login.html', page_title="Login")
 
-    return render_template('sign.html', page_title="Sign_up",status= status)
+
+
+@app.route('/sign_up', methods=['POST', 'GET'])
+def sign_up_post():
+    if request.method == 'POST' and "sign_name" in request.form:
+        name = request.form['sign_name']
+        password = request.form['sign_pass']
+
+        name_check = models.User.query.filter_by(name=name).first()
+
+        if name_check is None:
+            salt = os.urandom(32)
+            key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+
+            user = models.User(name=name, salt=salt, key=key)
+            db.session.add(user)
+            db.session.commit()
+            session['name'] = name
+            return redirect(url_for("home"))
+        else:
+            status = "Name Already Taken"
+            return render_template('sign.html', page_title="Sign_up",status= status)
+    else:
+        return render_template('sign.html', page_title="Sign_up")
 
 
 
@@ -97,14 +85,13 @@ def map():
 
 @app.route('/history')
 def history():
-
     return render_template('history.html', page_title="history")
 
 
-@app.route('/equipment')
-def equipment():
+@app.route('/famous')
+def famous():
 
-    return render_template('equipment.html', page_title="equipment")
+    return render_template('famous.html', page_title="famous")
 
 
 if __name__ == "__main__":
