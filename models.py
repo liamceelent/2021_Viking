88f0a4ee9be4d42244fd60c71a543db6034a5b1c
liam <liamceelent@gmail.com>
2021-07-05 12:05:15 +1200

    database changes

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from main import db
 
 class User(db.Model):
     __tablename__ = "User"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(225), unique=True)
     salt = db.Column(db.String(225))
     key = db.Column(db.String(225))
@@ -11,24 +11,24 @@ class User(db.Model):
 
 class Location(db.Model):
     __tablename__ = "Location"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(225), unique=True)
 
 class Faction(db.Model):
     __tablename__ = "Faction"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(225), unique=True)
 
 class Location_Faction(db.Model):
     __tablename__ = "Location_Faction"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     lid = db.Column(db.Integer, db.ForeignKey('Location.id'), nullable = False)
     fid = db.Column(db.Integer, db.ForeignKey('Faction.id'), nullable = False)
     period = db.Column(db.Integer)
 
 class Famous_Viking(db.Model):
     __tablename__ = "Famous_Viking"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(225), unique=True)
     age = db.Column(db.Integer)
     fid = db.Column(db.Integer, db.ForeignKey('Faction.id'), nullable = False)
@@ -36,9 +36,13 @@ class Famous_Viking(db.Model):
     kill = db.Column(db.Integer)
     rating = db.Column(db.Integer)
 
+    # def __repr__(self):
+    #     return(self.id, self.name, self.age, self.fid, self.wid, self.kill, self.rating)
+
+
 class Weapon(db.Model):
     __tablename__ = "Weapon"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(225), unique=True)
 
 Visted = db.Table('Visted', db.Model.metadata,
