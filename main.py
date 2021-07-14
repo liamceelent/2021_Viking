2072049f6a1fabdffb37d248fd94640822e0c75a
liam <liamceelent@gmail.com>
2021-07-14 23:01:25 +1200

    emailing system working

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, url_for, render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from random import randint
 
+import smtplib
 import hashlib
 import os
 
@@ -9,6 +11,8 @@ app = Flask(__name__)
 app.config.from_object(Config)  # applying all config to app
 db = SQLAlchemy(app)
 
+EMAIL_ADDRESS = "limct1232@gmail.com"
+EMAIL_PASSWORD = "vcquwfgmlnoobuvi"
 
 import models
 
@@ -56,29 +60,59 @@ def sign_up_post():
     if request.method == 'POST' and "sign_name" in request.form:
         name = request.form['sign_name']
         password = request.form['sign_pass']
+        email = request.form['sign_email']
 
         name_check = models.User.query.filter_by(name=name).first()
+        email_check = models.User.query.filter_by(email=email).first()
 
         if name_check is None:
-            salt = os.urandom(32)
-            key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+            if email_check is None:
+                salt = os.urandom(32)
+                key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
 
-            user = models.User(name=name, salt=salt, key=key)
-            db.session.add(user)
-            db.session.commit()
-            session['name'] = name
-            return redirect(url_for("home"))
+                user = models.User(name=name, salt=salt, key=key, email=email)
+                db.session.add(user)
+                db.session.commit()
+                session['name'] = name
+                return redirect(url_for("home"))
+            else:
+                status = "Email is Already Taken"
+                return render_template('sign.html', page_title="Sign_up",status= status)
         else:
-            status = "Name Already Taken"
+            status = "Name is Already Taken"
             return render_template('sign.html', page_title="Sign_up",status= status)
     else:
         return render_template('sign.html', page_title="Sign_up")
 
 
 
-@app.route('/forgot')
+@app.route('/forgot', methods=['POST', 'GET'])
 def forgot():
     current_user = session.get('name')
+
+    if request.method == 'POST' and "email" in request.form:
+        email = request.form['email']
+
+        email_check = models.User.query.filter_by(email=email).first()
+
+        if email_check is not None:
+            password_change = randint(10000, 99999)
+
+            with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+                smtp.ehlo()
+                smtp.starttls()
+                smtp.ehlo()
+                smtp.login(EMAIL_ADDRESS,EMAIL_PASSWORD)
+
+                subject = "your a loser"
+                body = f"your new password is {password_change}"
+
+                msg = f'subject:{subject}\n\n{body}'
+
+                smtp.sendmail("limct1232@gmail.com", email, msg)
+
+
+
     return render_template('forgot.html', page_title="forgot", user = current_user)
 
 
@@ -88,9 +122,9 @@ def map():
 
     location = models.Location.query.all()
 
-
-
-    return render_template('map.html', page_title="map", user = current_user, country = location)
+    viking_clans = models.Faction.query.all()
+    print(viking_clans)
+    return render_template('map.html', page_title="map", user = current_user, country = location, clan = viking_clans)
 
 
 @app.route('/history')
@@ -112,8 +146,8 @@ def user():
 
     user = models.User.query.filter_by(name = session['name']).first()
 
+
     image = user.image.name
-    print(image)
 
     return render_template('user.html', page_title="user", user = current_user, images = image)
 
@@ -124,6 +158,7 @@ def famous():
     famous_vikings = models.Famous_Viking.query.all()
     current_user = session.get('name')
     return render_template('famous.html', page_title="famous", vikings = famous_vikings, user = current_user)
+
 
 @app.route('/famous_click', methods=['POST', 'GET'])
 def famous_click():
